50fceed0335a3c5f69f61121b5fd9567fa979a4e
Alex Jurbert <alex.jurbert15@gmail.com>
2022-03-06 17:22:15 -0500

    Add realistic physic on ball and change gravity

--- Assets/script/RealisticPhysic.cs
+++ Assets/script/RealisticPhysic.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class RealisticPhysic : MonoBehaviour
 {
-    public Rigidbody rb;
+    private Rigidbody rb;
+
+    [SerializeField]
+    private Vector3 lastFrameVelocity;
+
+    [SerializeField]
+    public float minVelocity;
 
     // Start is called before the first frame update
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        lastFrameVelocity = rb.velocity;
+        minVelocity = 0;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(Physics.gravity * rb.mass);
+        lastFrameVelocity = rb.velocity;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var speed = lastFrameVelocity.magnitude;
+        var direction = Vector3.Reflect(lastFrameVelocity.normalized, collision.contacts[0].normal);
+        GetComponent<Rigidbody>().velocity = direction * (Mathf.Max(speed, minVelocity) / 4) * 3;
     }
 }
