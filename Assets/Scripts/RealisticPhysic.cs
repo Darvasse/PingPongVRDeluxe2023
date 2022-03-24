1eb65cc9a0a4b8789360e29c73bcc748653c064f
Alex Jurbert <alex.jurbert15@gmail.com>
2022-03-24 16:37:40 -0400

    Add LineCast on detection collider

--- Assets/Scripts/RealisticPhysic.cs
+++ Assets/Scripts/RealisticPhysic.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class RealisticPhysic : MonoBehaviour
 {
+    private LayerMask CastMask;
+
+    [SerializeField]
+    //La position de la balle au moment de la frame précédente
+    private Vector3 lastFramePosition;
+
+    [SerializeField]
+    //La position de la balle au moment de la frame actuelle
+    private Vector3 currentFramePosition;
+
+    [SerializeField]
+    //Le rigidbody de la balle
     private Rigidbody rb;
 
     [SerializeField]
-    private Vector3 currentFrameVelocity;
+    //La vitesse de la balle
+    private Vector3 velocity;
 
     [SerializeField]
     public float minVelocity;
@@ -15,21 +28,57 @@ public class RealisticPhysic : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
+        //On initialise la position de la balle à la frame précédente et actuelle
+        lastFramePosition = transform.position;
+        currentFramePosition = transform.position;
+        //On initialise le rigidbody de la balle
         rb = GetComponent<Rigidbody>();
-        currentFrameVelocity = rb.velocity;
+        //On initialise la vitesse de la balle
+        velocity = rb.velocity;
         minVelocity = 0;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        currentFrameVelocity = rb.velocity;
+        //On met à jour les positions de la balle aux différentes frames
+        lastFramePosition = currentFramePosition;
+        currentFramePosition = transform.position;
+        //On met à jour la vitesse de la balle
+        velocity = rb.velocity;
+
+        //On utilise un linecast pour savoir si la balle est en collision avec un objet
+        if(Physics.Linecast(lastFramePosition, currentFramePosition, out RaycastHit hit, CastMask))
+        {
+            //Si c'est une raquette
+            if(hit.collider.gameObject.tag == "Racket")
+            {
+                //On récupère la force de la raquette
+                float racketForce = hit.collider.gameObject.GetComponent<Racket_Force>().getForce();
+                //Si cette force est positive
+                if(racketForce > 0)
+                {
+                    //On applique la force de la raquette sur la balle
+                    rb.velocity = Vector3.Reflect(velocity, hit.normal) * (Mathf.Max(velocity.magnitude, 0) / 30) * racketForce;
+                }
+                else
+                {
+                    //Sinon on applque un force par défaut sur la balle
+                    rb.velocity = Vector3.Reflect(velocity, hit.normal) * (Mathf.Max(velocity.magnitude, 0) / 4) * 3;
+                }
+            }
+            //Sinon on applque un force par défaut sur la balle
+            else
+            {
+                rb.velocity = Vector3.Reflect(velocity, hit.normal) * (Mathf.Max(velocity.magnitude, 0) / 4) * 3;
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        var speed = currentFrameVelocity.magnitude;
-        var direction = Vector3.Reflect(currentFrameVelocity.normalized, collision.contacts[0].normal);
+        var speed = velocity.magnitude;
+        var direction = Vector3.Reflect(velocity.normalized, collision.contacts[0].normal);
         if (collision.gameObject.tag == "Racket")
         {
             float racketForce = collision.gameObject.GetComponent<Racket_Force>().getForce();
@@ -48,3 +97,5 @@ public class RealisticPhysic : MonoBehaviour
         }
     }
 }
+
+
